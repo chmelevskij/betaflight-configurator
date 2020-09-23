12acd0c0bd1d80984e9d844ddd6312a80cbc72e5
Tomas Chmelevskij <tomas.chmelevskij@clearscore.com>
2020-09-23 21:27:49 +0200

    Add Vue
    Setup vue with gulp and initial components:
    
    * Status Bar
    * Logo
    * Battery Legend

---
+++ src/vue/betaflight-logo/BetaflightLogo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="logo">
+    <div class="logo_text">
+      <span>
+        {{ $t("versionLabelConfigurator.message") }}: {{ configuratorVersion }}
+        <br />
+        <span v-if="firmwareVersion && firmwareId">
+          {{ $t("versionLabelFirmware.message") }}: {{ firmwareVersion }}
+          {{ firmwareId }}
+        </span>
+        <br />
+        <span v-if="hardwareId">
+          {{ $t("versionLabelTarget.message") }}: {{ hardwareId }}
+        </span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    configuratorVersion: {
+      type: String,
+      required: true,
+    },
+    firmwareVersion: {
+      type: String,
+    },
+    firmwareId: {
+      type: String,
+    },
+    hardwareId: {
+      type: String,
+    },
+  },
+};
+</script>
