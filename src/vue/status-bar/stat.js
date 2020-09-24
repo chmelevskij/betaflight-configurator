4c17b344799812b763f70a4b2149a7514efbfae9
Tomas Chmelevskij <tomas.chmelevskij@clearscore.com>
2020-09-24 23:10:18 +0200

    feat: add custom filter for stat % strippign

--- src/vue/status-bar/stat.js
+++ src/vue/status-bar/stat.js
@@ -2,9 +2,19 @@ import Vue from "../../node_modules/vue/dist/vue.esm.browser.js";
 
 Vue.component("stat", {
     props: ["message", "value", "unit"],
+    // a bit of a hack here to get around the current translations.
+    // vue i18n provides slightly different api for this. But
+    // it's also possible to provide custom formatter
+    filters: {
+        stripEnd(value) {
+            return value.replace(/\$1%/, "");
+        },
+    },
     template: `
         <div>
-            <span>{{ $t(message + ".message") }}</span> <span>{{ value }}</span><span v-if="unit">{{ unit }}</span>
+            <span>{{ $t(message + ".message") | stripEnd }}</span>
+            <span>{{ value }}</span>
+            <span v-if="unit">{{ unit }}</span>
         </div>
     `,
 });
