271c04c8f9311c818db812f92eddad86693d5940
Tomas Chmelevskij <t.chmelevskij@gmail.com>
2020-12-22 07:22:16 +0400

    feat: add `lint-staged`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-    extends: [ ],
     parserOptions: {
         ecmaVersion: 2020,
-        sourceType: 'module'
+        sourceType: "module"
     },
     env: {
         node: true,
         jquery: true,
         es2017: true,
         browser: true,
-        webextensions: true,
+        webextensions: true
     },
     rules: {
-        'no-trailing-spaces': 'error',
-        'eol-last': 'error'
-    },
+        "no-trailing-spaces": "error",
+        "eol-last": "error"
+    }
 };
