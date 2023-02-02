0881820f8cbb4324db75463f60a3bc221cafbad5
Tomas Chmelevskij <t.chmelevskij@gmail.com>
2023-02-02 20:10:28 +0100

    remove handroled debounce

--- src/js/tabs/osd.js
+++ src/js/tabs/osd.js
@@ -21,21 +21,6 @@ const FONT = {};
 const SYM = {};
 const OSD = {};
 
-function debounce(func, wait, immediate) {
-    let timeout;
-    return function() {
-        const context = this, args = arguments;
-        const later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        const callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-}
-
 SYM.loadSymbols = function() {
     SYM.BLANK = 0x20;
     SYM.VOLT = 0x06;
