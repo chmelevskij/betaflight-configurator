6d22a976f3c5f41c43b5d5fdf55ba373490d1caa
Tomas Chmelevskij <t.chmelevskij@gmail.com>
2023-08-13 14:23:35 +0300

    feat: load locales in vite build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,47 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
+import { readFileSync } from "node:fs";
+
+/**
+ * This is plugin to work around the file structure required nwjs.
+ * In future this can be dropped if we restructure folder structure
+ * to be more web friendly.
+ * @returns {import('vite').Plugin}
+ */
+function serveLocalesPlugin() {
+    return {
+        name: "serve-locales",
+        configureServer(server) {
+            return () => {
+                server.middlewares.use((req, res, next) => {
+                    if (req.url.startsWith("/locales/")) {
+                        // Extract the file path from the URL
+                        const filePath = req.url.replace(/^\/locales\//, "");
+                        const absolutePath = path.resolve(
+                            process.cwd(),
+                            "locales",
+                            filePath,
+                        );
+
+                        try {
+                            const fileContents = readFileSync(
+                                absolutePath,
+                                "utf-8",
+                            );
+                            res.end(fileContents);
+                        } catch (e) {
+                            // If file not found or any other error, pass to the next middleware
+                            next();
+                        }
+                    } else {
+                        next();
+                    }
+                });
+            };
+        },
+    };
+}
 
 export default defineConfig({
     test: {
@@ -12,12 +53,11 @@ export default defineConfig({
         environment: "jsdom",
         setupFiles: ["test/setup.js"],
     },
-    plugins: [vue()],
+    plugins: [vue(), serveLocalesPlugin()],
     root: "./src",
     resolve: {
         alias: {
             "/src": path.resolve(process.cwd(), "src"),
-            "/locales": path.resolve(process.cwd(), 'locales'),
         },
     },
 });
